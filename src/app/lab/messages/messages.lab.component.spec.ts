b15432052f2053485b5ef90a57f2b35d6d3f5b92
MoodyEG <moodyeg2011@yahoo.com>
2025-02-22 05:30:33 +0200

    Minor edits on the tests

--- src/app/lab/messages/messages.lab.component.spec.ts
+++ src/app/lab/messages/messages.lab.component.spec.ts
@@ -22,7 +22,6 @@ describe('2-message component integration testing:', () => {
 
   it('expect component template to be empty', () => {
     //Note: there is @if"messageService.messages.length" in line 1 in template
-    // ! Note, I edited the HTML
     fixture = TestBed.createComponent(MessagesComponentForLab);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -30,7 +29,7 @@ describe('2-message component integration testing:', () => {
     element = fixture.debugElement.nativeElement;
     // console.log(element.querySelector('h2')?.textContent);
 
-    expect(element.querySelector('h2')?.textContent).toContain('Messages');
+    expect(element.querySelector('h2')).toBeNull();
   });
 
   it('then expect div.msg to have the messages after setting it', () => {
