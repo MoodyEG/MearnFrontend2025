09030a0a1d7fbaacf323665d8066adde87949785
MoodyEG <moodyeg2011@yahoo.com>
2025-02-22 04:33:39 +0200

    Added frontend tests

--- src/app/lab/pipe/square.pipe.spec.ts
+++ src/app/lab/pipe/square.pipe.spec.ts
@@ -1,6 +1,15 @@
-describe("1-square pipe (class only) testing:",()=>{
+import { SquarePipeForLab } from './square.pipe';
 
-    it("expect to return 16 when passing 4",()=>{})
-    it("expect to return 9 when passing '3'",()=>{})
-    it("expect to return 'Not a number' when passing wrong parameter",()=>{})
-})
+describe('1-square pipe (class only) testing:', () => {
+  it('expect to return 16 when passing 4', () => {
+    expect(new SquarePipeForLab().transform(4)).toBe(16);
+  });
+
+  it("expect to return 9 when passing '3'", () => {
+    expect(new SquarePipeForLab().transform('3')).toBe(9);
+  });
+
+  it("expect to return 'not a number' when passing wrong parameter", () => {
+    expect(new SquarePipeForLab().transform('bl7')).toBe('not a number');
+  });
+});
